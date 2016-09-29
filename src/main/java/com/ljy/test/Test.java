942d2f09884e733b646b079690d43f357004c226
wangbl <wangbl1978@gmail.com>
2016-09-29 11:13:49 +0800

    Initial commit

---
+++ src/main/java/com/ljy/test/Test.java
@@ -0,0 +1,10 @@
+package com.ljy.test;
+
+/**
+ * Created by wangbl on 16/9/29.
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.println("Hello LJY,ljy,卢俊义!");
+    }
+}
